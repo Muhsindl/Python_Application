d59599bc8e261c80cad4d219aabff614e22ec4fb
Muhsin DOLU <muhsindolu06@gmail.com>
2023-12-10 21:31:43 +0300

    Araba sınıfı ile ilgili uygulama
    Araba sınıfı nesnesi oluşturulup hizlan ve yavasla metotları ile araç durumunun değiştirilmesi ve hizGosterge metodu ile bu durumu kullanıcı tarafından gözlemleyen uygulama

---
+++ Araba.py
@@ -0,0 +1,27 @@
+class Araba(object):
+    def __init__(self, marka, model, ):
+        self.marka = marka
+        self.model = model
+        self.hiz = 0
+
+    def hizGosterge(self):
+        print("Araç hızı: {}".format(self.hiz))
+
+    def hizlan(self, artis):
+        self.hiz += artis
+        print("Araç Hızı: {}".format(self.hiz))
+
+    def yavasla(self, azalis):
+        self.hiz -= azalis
+        print("Araç Hızı: {}".format(self.hiz))
+
+
+a1 = Araba("Ford", "Focus")
+a2 = Araba("Mercedes", "C-180")
+a3 = Araba("Audi", "A3")
+a1.hizlan(100)
+a2.hizlan(120)
+a3.hizlan(115)
+a1.hizGosterge()
+a2.hizGosterge()
+a3.hizGosterge()
