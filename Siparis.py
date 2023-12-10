a7be7f820dca57e992866bb5586fb9f2b3af7001
Muhsin DOLU <muhsindolu06@gmail.com>
2023-12-10 21:23:17 +0300

    Sipariş sınıf uygulaması
    Kullanıcıdan giriş değerleri olarak eklenecek ürün ile ilgili id, ürün fiyatı, stok sayısı ve alınacak ürüne dair numara, sipariş sayısı girdisi ile ürün fiyatını, stokta kalan ürün miktarını ve ürün alınıp alınamama durumunu kontrol edip ekrana yazdıran uygulama

---
+++ Siparis.py
@@ -0,0 +1,29 @@
+class Siparis(object):
+    def __init__(self,siparisNo,siparisFiyatı,stok,numara,siparisSayısı):
+        self.__siparisNo=siparisNo
+        self.siparisSayısı=siparisSayısı
+        self.siparisFiyatı=siparisFiyatı
+        self.stok=stok      
+        self.numara=numara
+    def siparisHesapla(self):
+        if self.siparisSayısı<= self.stok:
+            if self.numara==self.__siparisNo:
+                self.stok=self.stok-self.siparisSayısı
+                ucret=self.siparisFiyatı*self.siparisSayısı
+                print(ucret)
+            else:
+                print("Ürün stokta bulunamadı")
+        else:
+            print("Stokta o kadar ürün yok")
+
+#Kullanıcıdan eklenecek ürün girdileri alındı
+n=input("Eklenecek ürün Id giriniz: ")
+fiyat=int(input("Ürün Fiyatı giriniz:"))
+stokmiktarı=int(input("Stok Miktarı giriniz:"))
+
+#Kullanıcıdan alınacak ürün girdileri alındı
+num=input("Alınacak ürün Id giriniz:")
+sayi=int(input("Alınacak Miktar giriniz:"))
+
+s1=Siparis(n,fiyat,stokmiktarı,num,sayi)
+s1.siparisHesapla()
