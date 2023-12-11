5b6b6bc264def8ec207157924331e5a42211ce1e
Muhsin DOLU <muhsindolu06@gmail.com>
2023-12-11 22:33:27 +0300

    İki bilinmeyenli ikinci dereceden denklemin kökünü bulan uygulama
    Kullanıcıdan alınan kat sayıya göre iki bilinmeyenli ikinci dereceden denklemin kökünü bulan uygulama

---
+++ İkinciDereceKökBulma.py
@@ -0,0 +1,8 @@
+a=int(input("A değeri giriniz: "))
+b=int(input("B değeri giriniz: "))
+c=int(input("C değeri giriniz: "))
+d=(b**2)-(4*a*c)
+x1=((-1*b)+(d**1/2))/(2*a)
+x2=((-1*b)-(d**1/2))/(2*a)
+
+print("kök 1: ",x1,"\nkök 2:",x2)
