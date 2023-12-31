a4a5499521b91adf2f45b70aabc5d4537b15f60a
Muhsin DOLU <muhsindolu06@gmail.com>
2023-12-31 15:55:03 +0300

    Listede en büyük eleman bulma
    Kullanıcı tarafından oluşturulan listede büyük elemanı bulup ekrana yazdıran uygulama

--- ListedeBüyükElemanBulma.py
+++ ListedeBüyükElemanBulma.py
@@ -3,8 +3,8 @@ n=int(input("Eleman sayısı giriniz: "))
 for i in range(n):
     eleman = int(input(f"{i+1}. elemanı gir: "))
     dizi.append(eleman)
-minEleman=dizi[0]
+maxEleman=dizi[0]
 for i in dizi:
-    if i<minEleman:
-        minEleman=i
-print("En küçük Eleman:",minEleman)
+    if i>maxEleman:
+        maxEleman=i
+print("En büyük Eleman:",maxEleman)
