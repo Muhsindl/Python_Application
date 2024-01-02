eb6fb626c22e9a99217a784198f025325880d966
Muhsin DOLU <muhsindolu06@gmail.com>
2024-01-02 20:48:49 +0300

    İki boyutlu listede sol alt üçgen elemanını değiştirme
    Kullanıcı tarafından oluşturulan iki boyutlu listede sol alt üçgeni kullanıcının girdiği eleman ile değiştiren uygulama

---
+++ İkiBoyutluListedeSolAtIslem.py
@@ -0,0 +1,18 @@
+import numpy as np
+satır=int(input("Satır sayısı giriniz: "))
+sutun=int(input("Sutun sayısı giriniz: "))
+dizi=np.array([])
+
+for i in range(satır):
+    for j in range(sutun):
+        n=int(input("Eleman giriniz: "))
+        dizi=np.append(dizi,n)
+dizi=dizi.reshape((satır,sutun))
+
+eleman=int(input("Değiştirilecek eleman gir: "))
+for i in range(satır):
+    for j in range(sutun):
+        if i>=j: # i>=j or i>js
+            dizi[i][j]=eleman
+            
+print(dizi)
