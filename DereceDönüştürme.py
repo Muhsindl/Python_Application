09b490ba6d870960e0f9b156c37075f217049263
Muhsin DOLU <muhsindolu06@gmail.com>
2024-01-11 22:18:27 +0300

    Derece dönüştürme
    Kullanıcı tarafından alınan celcius (santigrat) derecelerini kelvin'e ve fahrenheit'a çeviren uygulama

---
+++ DereceDönüştürme.py
@@ -0,0 +1,7 @@
+c_derece=int(input("Celcius olarak derece giriniz: "))
+f_derece=(c_derece*1.8000)+32
+k_derece=c_derece+273.15
+
+print("Celcius: ",c_derece)
+print("Fahrenheit: ",f_derece)
+print("Kelvin: ",k_derece)
