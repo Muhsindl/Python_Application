fb2ce747b6c31f2b39ebea7ea35e26d984f17544
Muhsin DOLU <muhsindolu06@gmail.com>
2024-01-24 00:09:16 +0300

    Kare çizdirme uygulaması
    Kullanıcı tarafından girilen ilk , son değerlerine göre (Matplotlib) kütüphanesi kullanarak kare çizdiren uygulama

--- KareCizdirme.py
+++ KareCizdirme.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
-x=[0,1,1,0,0]
-y=[0,0,1,1,0]
+ilkDeger=int(input("İlk değer giriniz: "))
+ikinciDeger=int(input("İkinci değer giriniz: "))
+
+x=[ilkDeger,ikinciDeger,ikinciDeger,ilkDeger,ilkDeger]
+y=[ilkDeger,ilkDeger,ikinciDeger,ikinciDeger,ilkDeger]
 
 plt.plot(x,y)
 plt.show()
