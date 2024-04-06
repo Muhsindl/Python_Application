62f2ea783eea1e451d2f362632474faa56708948
Muhsin DOLU <muhsindolu06@gmail.com>
2024-04-06 20:50:48 +0300

    Yaklaşık karekök hesaplama uygulaması
    Kullanıcı tarafından girilen bir sayının yaklaşık karekökünü hesaplayan uygulama

---
+++ YaklasikKarekokUygulamasi.py
@@ -0,0 +1,6 @@
+#Sayı girdisi alınır
+sayi=int(input("Sayı giriniz: "))
+#Girilen sayının karekökü alınır
+yaklasikKarekok=sayi**(1/2)
+#Sonuç ekrana yazdırılır
+print(f"{sayi}\'nın yaklaşık karekökü: {yaklasikKarekok}")
