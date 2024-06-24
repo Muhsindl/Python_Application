d0b47a20dc1dac2bc59140a2fcfa524c5bd03f12
Muhsin DOLU <muhsindolu06@gmail.com>
2024-06-24 23:12:26 +0300

    Update PalindromSayı.py

--- PalindromSayı.py
+++ PalindromSayı.py
@@ -1,7 +1,10 @@
+#Kullanıcıdan sayı alınması
 n0=input("Sayı giriniz: ")
+#Sayının terse çevrilmesi
 n1=n0[::-1]
 int(n0)
 int(n1)
+#Kıyaslama yapılması
 if n0==n1:
     print("Sayı palindrom sayıdır.")
 else:
