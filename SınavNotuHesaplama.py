6edf9156b23d041375ea09dba23b90d1c03c7bbd
Muhsin DOLU <muhsindolu06@gmail.com>
2024-07-01 22:47:24 +0300

    Update SınavNotuHesaplama.py

--- SınavNotuHesaplama.py
+++ SınavNotuHesaplama.py
@@ -2,8 +2,9 @@
 dogru=int (input("Doğru sayınızı giriniz: "))
 yanlis=int(input("Yanlış sayınızı giriniz: "))
 puan=0
-
+#Doğru bilinen 10 puan etki etmektedir.
 dogrupuan=dogru*10
+#Yanlış bilinen sorular -5 puan etki etmektedir.
 yanlispuan=yanlis*-5
 
 puan=dogrupuan+yanlispuan
